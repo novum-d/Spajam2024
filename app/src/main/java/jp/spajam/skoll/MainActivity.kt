f73185c0c7b732994ab988703d3ce08370b88d2b
TomokiHamada <t.hamada@fenrir.co.jp>
2024-10-06 03:37:56 +0900

    🏗️ chore: アプリアイコンを追加

--- app/src/main/java/jp/spajam/skoll/MainActivity.kt
+++ app/src/main/java/jp/spajam/skoll/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import jp.spajam.skoll.ui.navigation.NavGraph
 import jp.spajam.skoll.ui.theme.Spajam2024Theme
@@ -11,6 +12,7 @@ import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
