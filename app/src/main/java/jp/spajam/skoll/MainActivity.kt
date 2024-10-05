720c79af838cc5621b10f2689644043d097de09c
TomokiHamada <t.hamada@fenrir.co.jp>
2024-10-05 15:33:34 +0900

    ✨ feat: イベントID検索画面のレイアウトを追加

--- app/src/main/java/jp/spajam/skoll/MainActivity.kt
+++ app/src/main/java/jp/spajam/skoll/MainActivity.kt
@@ -4,13 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.LocationOn
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import jp.spajam.skoll.ui.theme.Spajam2024Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +34,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Spajam2024Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Text("", modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +42,38 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun EventIdSearchPage() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.padding(horizontal = 48.dp, vertical = 60.dp),
+    ) {
+        Icon(
+            imageVector = Icons.Outlined.LocationOn,
+            contentDescription = null,
+            modifier = Modifier.size(150.dp)
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Column {
+            Text("イベント ID")
+            Spacer(modifier = Modifier.height(16.dp))
+            TextField("", {})
+        }
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Button({}, modifier = Modifier.fillMaxWidth()) {
+            Text("つぎへ")
+        }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun EventIdSearchPagePreview() {
     Spajam2024Theme {
-        Greeting("Android")
+        EventIdSearchPage()
     }
 }
